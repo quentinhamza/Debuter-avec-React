fdb59d21acb7cb875ce9cd547fbc7d7e0c8655fc
Quentin Hamza <quentinhamzaent@gmail.com>
2024-07-01 20:26:57 +0200

    ze

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -19,6 +19,7 @@ function ShoppingList() {
 				{plantList.map((plant) => (
 					<li key={plant.id} className='lmj-plant-item'>
 						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
 					</li>
 				))}
 			</ul>
